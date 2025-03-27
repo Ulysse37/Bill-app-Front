96215fac59bde87e9cfe33acf201d3675e5d3265
Ulysse37 <ulysse.courtet@hotmail.fr>
2025-03-27 14:50:34 +0100

    [ Bug Hunt : DashBoard ] - Fix avec un .off

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -91,30 +91,35 @@ export default class {
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} alt="Bill"/></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
-
-  handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+  
+  handleEditTicket(e, bill, bills) { // méthode originale mais booléen
+    if (this.isEditing === undefined || this.id !== bill.id) {
+      this.isEditing = false;
+    }
+    if (this.id === undefined || this.id !== bill.id) {
+      this.id = bill.id;
+    }
+    if (!this.isEditing) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
-    } /* else {  //! Si suppr = fix bug mais ajout fail test dashboard : Then, big bill Icon should Appear
+      this.isEditing = true;
+    } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
-
       $('.dashboard-right-container div').html(`
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
-    } */
+      this.isEditing = false;
+    }
+
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
+    console.log("editing", this.isEditing);
   }
 
   handleAcceptSubmit = (e, bill) => {
@@ -137,33 +142,7 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
-  /* handleShowTickets(e, bills, index) { // Méthode originale 
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
-    }
-
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-    console.log("Counter", this.counter);
-    console.log("Bills :", bills);
-    console.log("index :", index);
-    console.log("getStatus :", getStatus(index));
-    return bills
-
-  } */
-
-  /* handleShowTickets(e, bills, index) { // méthode originale mais booléen
+  handleShowTickets(e, bills, index) { // méthode originale mais booléen
     if (this.index === undefined || this.index !== index) {
       this.index = index;
       this.isOpen = false;
@@ -182,43 +161,15 @@ export default class {
     }
   
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills));
-    });
+      $(`#open-bill${bill.id}`).off('click').click((e) => this.handleEditTicket(e, bill, bills));
+    }); //! Ajout du .off pour enlever les événements de click précédemment crées - [ Bug Hunt : DashBoard]
 
     console.log("IsOpen", this.isOpen);
-    //console.log("Bills :", bills);
     console.log("index :", index);
-    //console.log("getStatus :", getStatus(index));
+
     return bills;
-  } */
-
- handleShowTickets(e, bills, index) { // METHODE QUI MARCHE SI JE FERME TOUT ET REOUVRE
-  const isOpen = this.isOpen[index];
-  if (isOpen === undefined || isOpen === false) {
-    this.isOpen[index] = true;
-  } else {
-    this.isOpen[index] = false;
   }
-
-  if (this.isOpen[index]) { // Si la catégorie est ouverte affiche les tickets
-    $(`#arrow-icon${index}`).css({ transform: 'rotate(0deg)' });
-    $(`#status-bills-container${index}`)
-      .html(cards(filteredBills(bills, getStatus(index))));
-  } else { // sinon affiche rien
-    $(`#arrow-icon${index}`).css({ transform: 'rotate(90deg)' });
-    $(`#status-bills-container${index}`)
-      .html("");
-  }
-
-  bills.forEach(bill => {
-    $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills));
-  });
-
-  console.log("IsOpen", this.isOpen);
-  console.log("index :", index);
-  return bills;
-}
-
+  
   getBillsAllUsers = () => {
     if (this.store) {
       return this.store
